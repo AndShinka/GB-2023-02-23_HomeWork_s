35454f310c0af489b7c0d4ab76b890678edc6c1d
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-03-02 12:31:35 +0300

    1. Показать натуральные числа от M до N, N и M заданы

---
+++ 01_task_HW_2023-02-23/Program.cs
@@ -0,0 +1,17 @@
+﻿// ### 1. Показать натуральные числа от M до N, N и M заданы
+// 01_task_HW_2023-02-23
+
+void PrintNaturalNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNaturalNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+
+int numberM = 11;
+int numberN = 30;
+
+Console.Clear();
+Console.WriteLine($"1. Показать натуральные числа от M до N, N и M заданы, M = {numberM}, N = {numberN}" !);
+PrintNaturalNumber(numberM, numberN);
+Console.WriteLine();
