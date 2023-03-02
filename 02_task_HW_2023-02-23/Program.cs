d745dd14629184e5433f6869adaad49d416038f7
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-03-02 13:06:10 +0300

    2. Найти сумму элементов от M до N, N и M заданы

---
+++ 02_task_HW_2023-02-23/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Найти сумму элементов от M до N, N и M заданы
+// 02_task_HW_2023-02-23
+
+int SumNaturalNumber(int m, int n)
+{
+    if (n == m) return n;
+    return m + SumNaturalNumber(m + 1, n);
+}
+
+int numberM = 1;
+int numberN = 5;
+
+Console.Clear();
+Console.WriteLine($"2. Найти сумму элементов от M до N, N и M заданы, M = {numberM}, N = {numberN}" !);
+Console.WriteLine();
+Console.WriteLine($"Cумма элементов от M = {numberM} до  N = {numberN} равна {SumNaturalNumber(numberM, numberN)}" !);
+
+Console.WriteLine();
